825f04e3616aa3da74928d383733377b6e64de1c
Rohid <rohidul209@gmail.com>
2023-04-02 11:57:46 +0600

    constants

---
+++ src/utils/constants.ts
@@ -0,0 +1,12 @@
+export const INTRO_SCREEN_01 = {
+  title: "Connect With Creators Easily",
+  description: "Thank you for choosing us, we can save your lovely time.",
+};
+export const INTRO_SCREEN_02 = {
+  title: "Get Inspiration From Creators",
+  description: "Find your favorite creators and get insperation from them",
+};
+export const LOG_IN_SCREEN = {
+  title: "Let's\nGet Started",
+  description: "To register for an account, kindly enter your details",
+};
